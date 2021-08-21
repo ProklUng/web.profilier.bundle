78534083b799fe20e0e504b8c151349e88951a9d
ProklUng <funtik21975@gmail.com>
2021-08-21 14:36:57 +0300

    Удаление лишнего

--- src/Bundle/Resources/config/services.php
+++ src/Bundle/Resources/config/services.php
@@ -4,22 +4,10 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Prokl\WebProfilierBundle\ContentSecurityPolicyHandler;
 use Prokl\WebProfilierBundle\NonceGenerator;
-use Prokl\WebProfilierBundle\ProfilerController;
 use Prokl\WebProfilierBundle\WebDebugToolbarListener;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
-        ->set('web_profiler.controller.profiler', ProfilerController::class)
-        ->public()
-        ->args([
-            service('router')->nullOnInvalid(),
-            service('transformers.bag'),
-            service('profiler')->nullOnInvalid(),
-        ])->tag('controller.service_arguments')
-        ->public()
-
-        ->alias(ProfilerController::class, 'web_profiler.controller.profiler')->public()
-
         ->set('web_profiler.csp.handler', ContentSecurityPolicyHandler::class)
         ->args([
             inline_service(NonceGenerator::class),
